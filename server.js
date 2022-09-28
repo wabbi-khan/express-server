63ae64750e95c3982c88c4a08fc5ae114bf1a30d
wabbi-khan <kwahab789@gmail.com>
2022-09-28 12:53:47 +0500

    creater server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require("express");
+const app = express();
+
+// ? creating server with the help of listen
+const PORT = process.env.PORT || 4000;
+app.listen(4000, () => {
+    console.log(`Listening on Port ${PORT}`);
+});
