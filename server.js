09f4788df9f4cb3ad4d9969988e003815f7e7223
wabbi-khan <kwahab789@gmail.com>
2022-09-28 13:19:15 +0500

    ok

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 // ? creating server with the help of listen
 const PORT = process.env.PORT || 4000;
+
+// ? creating routes
+app.get("/", (req, res) => {});
+
 app.listen(4000, () => {
     console.log(`Listening on Port ${PORT}`);
 });
