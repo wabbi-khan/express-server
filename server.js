6e8481892f3651db34698994c34ced0771183d47
wabbi-khan <kwahab789@gmail.com>
2022-09-28 13:35:28 +0500

    working

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // ? creating routes
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+    res.send("Hello from Express ...");
+});
 
 app.listen(4000, () => {
     console.log(`Listening on Port ${PORT}`);
