65ff801d329ac6ffa429266ac4621515270346bd
wabbi-khan <kwahab789@gmail.com>
2022-10-05 13:45:52 +0500

    react app

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return ( <
+        ul className = 'list-group' >
+        <
+        li className = 'list-group-item' > A second item < /li>{" "} <
+        /ul>
+    );
+};
+
+ReactDOM.render( < App / > , document.getElementById("app"));
